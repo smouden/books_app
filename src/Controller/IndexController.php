7fcbf387677f27f44d9b973ae75e30cffb3af42e
saad mouden <saadmouden02@gmail.com>
2025-02-09 22:33:47 +0100

    aya

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    // hello is saad 
+    // hello is aya
     #[Route('/test', name: 'app_index')]
     public function index(): Response
     {
