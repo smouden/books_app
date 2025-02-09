12bbe911829f0a1c562259c63cb4f1890c4d826a
saad mouden <saadmouden02@gmail.com>
2025-02-09 22:20:04 +0100

    modifs

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,5 +14,7 @@ final class IndexController extends AbstractController
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+
+        
     }
 }
